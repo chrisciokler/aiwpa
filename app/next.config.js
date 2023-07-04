64d82fe5384f0a0f9bf94308e8d122930273104c
chrisciokler <christianpradociokler@gmail.com>
2023-07-04 15:52:55 -0400

    latest

--- app/next.config.js
+++ app/next.config.js
@@ -3,12 +3,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  optimizeFonts: true,
-
-  experimental: {
-    optimizeCss: true
-  },
-
   compiler: {
     removeConsole: isProduction,
   },
