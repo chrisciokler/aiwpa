477a16f76ff4c76318906d6922961e38b810af69
Christian Misael Prado Ciokler <72030877+chrisciokler@users.noreply.github.com>
2024-04-09 10:15:45 -0400

    adding new theme handler

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,10 +1,10 @@
-'use client';
 import { isProduction } from '@/constants';
 import Head from 'next/head';
 import Script from 'next/script';
 // import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 export const Analytics = () => {
+  console.log('🚀 | Analytics | isProduction:', isProduction)
 
   return (
     <>
@@ -12,7 +12,7 @@ export const Analytics = () => {
         {isProduction && (
           <>
             {/* <Script src="../scripts/hotjar.js" strategy="afterInteractive" /> */}
-            <Script defer src="https://analytics.us.umami.is/script.js" data-cache="true" data-website-id="398f4887-88ef-47f0-9d7a-3f5423b7c6dc" strategy="afterInteractive"></Script>
+            <script async defer src="https://analytics.us.umami.is/script.js" data-cache="true" data-website-id="398f4887-88ef-47f0-9d7a-3f5423b7c6dc" ></script>
           </>
         )}
       </Head>
